3a2da1114c37205459fb9ade5ca4071ba9599275
Volodymyr Nerovnia <nerv@i.ua>
2022-01-25 16:07:42 +0200

    Last update:   - del project   - new project

--- tasks/del_project.js
+++ tasks/del_project.js
@@ -5,23 +5,31 @@
 */
 
 const gulp = require('gulp');
-const fs = require('fs-extra');
+const fs = require('fs');
+//const fs = require('fs-extra');
 
 const cfg_path = './prj-conf.json'
 
 gulp.task('del-project', function (done) {
-  const cfg_obj = fs.readJsonSync(cfg_path, { throws: false });
+  const cfg_obj = JSON.parse(fs.readFileSync(cfg_path));
   if(cfg_obj !== null) {
     let act_arg = '';
     for(let arg of process.argv) {
       if ((arg.length != 0) && (arg.substring(0,2) != '--') && (act_arg === '--prj')) {
-        if(cfg_obj.prj.all.some(e => e.name === arg)) {
-          cfg_obj.prj.cfg.arr_src_dir.forEach(element => fs.removeSync(cfg_obj.prj.cfg.prj_dir + element + '/' + arg));
-          cfg_obj.prj.all.splice(cfg_obj.prj.all.findIndex((element) => element.name === arg),1);
-          fs.writeJsonSync(cfg_path, cfg_obj)        
+        let cfg_projects = JSON.parse(fs.readFileSync(cfg_obj.prj.cfg.projects_cfg));
+        if(cfg_projects) {
+          if(cfg_projects.all.some(e => e.name === arg)) {
+            // Delete project directories
+            cfg_obj.prj.cfg.arr_src_dir.forEach(element => fs.rmSync(cfg_obj.prj.cfg.prj_dir + element + '/' + arg, {recursive: true}));
+            // Delete project name from config file
+            cfg_projects.all.splice(cfg_projects.all.findIndex((element) => element.name === arg),1);
+            fs.writeFileSync(cfg_obj.prj.cfg.projects_cfg, JSON.stringify(cfg_projects));
+          } else {
+            console.log('Project with name "' + arg + '" not exist!')
+          }  
         } else {
-          console.log('Project with name "' + arg + '" not exist!')
-        }  
+          console.log('Project file ' + cfg_obj.prj.cfg.projects_cfg + ' not exist!');
+        }
       }
       if(arg.substring(0,2) === '--') {
         act_arg = arg;
--- tasks/new_project.js
+++ tasks/new_project.js
@@ -5,24 +5,32 @@
 */
 
 const gulp = require('gulp');
-const fs = require('fs-extra');
+const fs = require('fs');
 
-const cfg_path = './prj-conf.json'
+const cfg_path = './prj-conf.json';
   
 gulp.task('new-project', function (done) {
-  const cfg_obj = fs.readJsonSync(cfg_path, { throws: false });
+  const cfg_obj = JSON.parse(fs.readFileSync(cfg_path, { throws: false }));
   if(cfg_obj !== null) {
     let act_arg = '';
     for(let arg of process.argv) {
       if ((arg.length != 0) && (arg.substring(0,2) != '--') && (act_arg === '--prj')) {
-        if(!cfg_obj.prj.all.some(e => e.name === arg)) {
+        // Add project to config file
+        let cfg_projects = "";
+        try {
+          cfg_projects = JSON.parse(fs.readFileSync(cfg_obj.prj.cfg.projects_cfg));
+        } catch (err) {  
+          cfg_projects = {
+            all: []
+          };
+        }
+        if(!cfg_projects.all.some(e => e.name === arg)) {
           // Create directories for complete project
-          cfg_obj.prj.cfg.arr_src_dir.forEach(element => fs.ensureDirSync(cfg_obj.prj.cfg.prj_dir + element  + '/'  + arg));
-          // Add project to config file
-          cfg_obj.prj.all.push({
-              name: arg
+          cfg_obj.prj.cfg.arr_src_dir.forEach(element => fs.mkdirSync(cfg_obj.prj.cfg.prj_dir + element  + '/'  + arg));
+          cfg_projects.all.push({
+            name: arg
           });
-          fs.writeJsonSync(cfg_path, cfg_obj);
+          fs.writeFileSync(cfg_obj.prj.cfg.projects_cfg, JSON.stringify(cfg_projects), {flag: 'w'});
           // Initialise a routes object and create a start routes file
           let rout_obj = {routes:[]};
           rout_obj.prj_name = arg;
@@ -31,7 +39,7 @@ gulp.task('new-project', function (done) {
           route.route = cfg_obj.prj.cfg.start_route + arg;
           route.path = "proj-dir/" + arg + "/index"
           rout_obj.routes.push(route);
-          fs.writeJsonSync(cfg_obj.prj.cfg.prj_dir + cfg_obj.prj.cfg.prj_route_path + arg + '/' + cfg_obj.prj.cfg.prj_route_file, rout_obj);
+          fs.writeFileSync(cfg_obj.prj.cfg.prj_dir + cfg_obj.prj.cfg.prj_route_path + arg + '/' + cfg_obj.prj.cfg.prj_route_file, JSON.stringify(rout_obj));
           // Copy views templates
           let tmpl_views_dir = cfg_obj.prj.cfg.tmpl_dir + "views";
           let tmpl_dir = fs.readdirSync(tmpl_views_dir);
